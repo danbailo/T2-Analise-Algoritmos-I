6c8a62f28b67f6d7f83622cac4580fc815e73a0b
Daniel Bailo <danbailoufms@gmail.com>
2019-05-21 12:27:13 -0400

    configurado pra gerar o arquivo e o plot

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from knapsack import Knapsack, read_instances, organize_instances
+import json
+
 if __name__ == "__main__":
-    pass
+    all_instances = read_instances('./instancias/')
+    number_items, weight_max, values_items, weight_items = organize_instances(all_instances)
+    result_iterative, time_iterative, result_recursive, time_recursive = \
+    Knapsack().get_result(all_instances, number_items, weight_max, values_items, weight_items)
+
+    data = {}
+    k = 0
+    for instance in all_instances:
+        data[instance] = {
+            'result iterative':result_iterative[k], 'time iterative':time_iterative[k],\
+            'result recursive':result_recursive[k], 'time recursive':time_recursive[k]}
+        k += 1
+
+    with open('result.json','w') as file: file.write(json.dumps(data,indent=4)) 
--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ def plot_iterative(name_instance, time_iterative, result_iterative):
     plt.ylabel('Resultado')
     plt.scatter(time_iterative, result_iterative, marker='.', color='k',label='Instâncias')
     for i, txt in enumerate(name_instance):
-        plt.annotate(txt, (time_iterative[i], result_iterative[i]))
+        plt.annotate(txt, (time_iterative[i], result_iterative[i]), fontsize=7)
     plt.plot(time_iterative, result_iterative, color='orange')
     plt.legend()
     plt.grid()
@@ -31,25 +31,131 @@ def plot_iterative(name_instance, time_iterative, result_iterative):
     plt.savefig('./img/iterative_sorted.pdf')
     plt.show()  
 
-    plt.title('Instância/Resultado')
+    plt.title('Instância/Resultado - Iterativo')
     plt.xlabel('Instância')
     plt.ylabel('Resultado')
     plt.plot(name_instance, result_iterative, color='orange')
     plt.xticks(rotation=90, ha='right',fontsize=7)
     # plt.legend()
     plt.grid()
-    plt.savefig('./img/instacia_resultado.pdf')
+    plt.savefig('./img/instancia_iterative_recursive.pdf')
     plt.show()      
 
-    plt.title('Instância/Tempo')
+    plt.title('Instância/Tempo - Iterativo')
     plt.xlabel('Tempo/s')
     plt.ylabel('Resultado')
     plt.plot(name_instance, time_iterative, color='orange')
     plt.xticks(rotation=90, ha='right',fontsize=7)
     # plt.legend()
     plt.grid()
-    plt.savefig('./img/instacia_tempo.pdf')
+    plt.savefig('./img/instacia_tempo_iterative.pdf')
     plt.show()  
+
+def plot_recursive(name_instance, time_recursive, result_recursive):
+    time_recursive_sorted = sorted(time_recursive)
+    plt.style.use('default')
+    plt.title('Resultado do algoritmo recursivo')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.scatter(time_recursive, result_recursive, marker='.', color='k',label='Instâncias')
+    for i, txt in enumerate(name_instance):
+        plt.annotate(txt, (time_recursive[i], result_recursive[i]))
+    plt.plot(time_recursive, result_recursive)
+    plt.legend()
+    plt.grid()
+
+    plt.savefig('./img/recursive.pdf')
+    plt.show()
+
+    plt.title('Resultado do algoritmo iterativo, ordenado')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.scatter(time_recursive_sorted, result_recursive, marker='.', color='k',label='Instâncias')
+    for i, txt in enumerate(name_instance):
+        plt.annotate(txt, (time_recursive_sorted[i], result_recursive[i]), fontsize=7)    
+    plt.plot(time_recursive_sorted, result_recursive)
+    plt.grid()
+    plt.savefig('./img/recursive_sorted.pdf')
+    plt.show()  
+
+    plt.title('Instância/Resultado - Recursivo')
+    plt.xlabel('Instância')
+    plt.ylabel('Resultado')
+    plt.plot(name_instance, result_recursive)
+    plt.xticks(rotation=90, ha='right',fontsize=7)
+    # plt.legend()
+    plt.grid()
+    plt.savefig('./img/instacia_resultado_recursive.pdf')
+    plt.show()      
+
+    plt.title('Instância/Tempo - Recursivo')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.plot(name_instance, time_recursive)
+    plt.xticks(rotation=90, ha='right',fontsize=7)
+    # plt.legend()
+    plt.grid()
+    plt.savefig('./img/instacia_tempo_recursive.pdf')
+    plt.show()  
+
+def plot_it_vs_rec(name_instance, time_iterative, result_iterative,time_recursive, result_recursive):
+    time_iterative_sorted = sorted(time_iterative)
+    time_recursive_sorted = sorted(time_recursive)
+
+    plt.style.use('default')
+    plt.title('Resultado das duas versões do algoritmo')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.scatter(time_iterative, result_iterative, marker='.', color='c',label='Iterativo')
+    plt.scatter(time_recursive, result_recursive, marker='.', color='m',label='Recursivo')
+    for i, txt in enumerate(name_instance):
+        plt.annotate(txt, (time_iterative[i], result_iterative[i]),fontsize=7)
+        plt.annotate(txt, (time_recursive[i], result_recursive[i]),fontsize=7)
+    plt.plot(time_iterative, result_iterative, label='Iterativo')
+    plt.plot(time_recursive, result_recursive, label='Recursivo')
+    plt.legend()
+    plt.grid()
+
+    plt.savefig('./img/iterative_recursive.pdf')
+    plt.show()
+
+    plt.title('Resultado das duas versões do algoritmo, ordenado')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.scatter(time_iterative_sorted, result_iterative, marker='.', color='c',label='Iterativo')
+    plt.scatter(time_recursive_sorted, result_recursive, marker='.', color='m',label='Recursivo')
+    for i, txt in enumerate(name_instance):
+        plt.annotate(txt, (time_iterative_sorted[i], result_iterative[i]),fontsize=7)    
+        plt.annotate(txt, (time_recursive_sorted[i], result_recursive[i]),fontsize=7)  
+
+    plt.plot(time_iterative_sorted, result_iterative, label='Iterativo')
+    plt.plot(time_recursive_sorted, result_recursive, label='Recursivo')
+    plt.legend()
+    plt.grid()
+    plt.savefig('./img/iterative_recursive_sorted.pdf')
+    plt.show()  
+
+    plt.title('Instância/Resultado - Iterativo vs Recursivo')
+    plt.xlabel('Instância')
+    plt.ylabel('Resultado')
+    plt.plot(name_instance, result_iterative, label='Iterativo')
+    plt.plot(name_instance, result_recursive, label='Recursivo')
+    plt.xticks(rotation=90, ha='right',fontsize=7)
+    plt.legend()
+    plt.grid()
+    plt.savefig('./img/instancia_result_iterative_recursive.pdf')
+    plt.show()      
+
+    plt.title('Instância/Tempo - Iterativo')
+    plt.xlabel('Tempo/s')
+    plt.ylabel('Resultado')
+    plt.plot(name_instance, time_iterative, label='Iterativo')
+    plt.plot(name_instance, time_recursive, label='Recursivo')
+    plt.xticks(rotation=90, ha='right',fontsize=7)
+    plt.legend()
+    plt.grid()
+    plt.savefig('./img/instancia_tempo_iterative_recursive.pdf')
+    plt.show()      
 
 if __name__ == "__main__":
     with open("result.json") as f:
@@ -61,68 +167,6 @@ if __name__ == "__main__":
     result_recursive = list(element['result recursive'] for element in result.values())
     time_recursive = list(element['time recursive'] for element in result.values())
 
-    plot_iterative(name_instance, time_iterative, result_iterative)
-
-    plt.title('Resultado do algoritmo recursivo')
-    plt.xlabel('Tempo/s')
-    plt.ylabel('Resultado')
-    plt.plot(time_recursive, result_recursive)
-    plt.xticks(rotation=90, ha='right')
-
-    plt.legend()
-    plt.grid()
-    plt.savefig('./img/recursive.pdf')
-    # plt.show()
-
-    plt.title('Resultado do algoritmo recursivo, ordenado')
-    plt.xlabel('Tempo/s')
-    plt.ylabel('Resultado')
-    plt.plot(sorted(time_recursive), result_recursive)
-    plt.xticks(rotation=90, ha='right')
-    plt.grid()
-    plt.savefig('./img/recursive_sorted.pdf')
-    # plt.show()   
-
-    ############################################################################
-
-    plt.title('Iterative vs Recursive')
-    plt.xlabel('Tempo/s')
-    plt.ylabel('Resultado')
-    plt.plot(time_iterative, result_iterative, label='Iterativo')
-    plt.plot(time_recursive, result_recursive, label='Recursivo')
-    plt.xticks(rotation=90, ha='right')
-    plt.legend()
-    plt.grid()
-    plt.savefig('./img/iterative_vs_recursive.pdf')
-    # plt.show()   
-
-    plt.title('Iterative vs Recursive SORTED')
-    plt.xlabel('Tempo/s')
-    plt.ylabel('Resultado')
-    plt.plot(sorted(time_iterative), result_iterative, label='Iterativo')
-    plt.plot(sorted(time_recursive), result_recursive, label='Recursivo')
-    plt.xticks(rotation=90, ha='right')
-    plt.legend()
-    plt.grid()
-    plt.savefig('./img/iterative_vs_recursive_sorted.pdf')
-    # plt.show()   
-
-
-
-
-    # plt.title('Trabalho II')
-    # plt.xlabel('Tempo/s')
-    # plt.ylabel('Resultado')
-
-    # plt.bar(time, result, width=0.01,edgecolor='black')
-    # plt.xticks(time, names, rotation=90)
-
-    # plt.grid()
-    # plt.legend()
-    # plt.show()
-
-    # x = [0, 1, 2, 3, 4, 20] #onde esta as barras
-    # y = [2, 3, 5, 3, 7, 9] #tamanho das barras, eixo y
-
-    # plt.bar(x,y)
-    # plt.show()
+    # plot_iterative(name_instance, time_iterative, result_iterative)
+    # plot_recursive(name_instance, time_recursive, result_recursive)
+    plot_it_vs_rec(name_instance, time_iterative, result_iterative,time_recursive, result_recursive)
