13ab8539bea6a06f2ecf33009eac89a0139aee2a
Daniel Bailo <danbailoufms@gmail.com>
2019-05-27 03:01:20 -0400

    classe criada para organizar os arquivos e updated

--- src/result.py
+++ src/result.py
@@ -20,31 +20,32 @@ def number_solutions(n):
 
 def get_solutions(all_instances, number_items, weight_max, values_items, weight_items):
     try:
-        with open('./number_of_results.txt', 'r') as result_txt: n = int(result_txt.readline())
-    except FileNotFoundError as err:
+        with open('./number_of_results.txt', 'r') as result_txt: n_result = int(result_txt.readline())
+    except FileNotFoundError:
         print("Please, run '$ python3 main.py n_sol 1' by default, before execute 'get_sol'")
         exit(-1)
-    except UnboundLocalError as err:
+    except UnboundLocalError:
         print("Please, run '$ python3 main.py n_sol 1' by default, before execute 'get_sol'")
         exit(-1)
-    if n == 0: 
+    if n_result == 0: 
         print('\n0 results? OK! Done...\n')
         return False
     print('\nGenerating result...')
-    for n in range(1, n+1):
+    for n in range(1, n_result+1):
         result_bottomUp, time_bottomUp, result_topDown, time_topDown = \
         Knapsack().get_result(all_instances, number_items, weight_max, values_items, weight_items)
         data = {}
         k = 0
         for instance in all_instances:
             data[instance] = {
-                'result bottomUp':result_bottomUp[k], 'time bottomUp':time_bottomUp[k],\
-                'result topDown':result_topDown[k], 'time topDown':time_topDown[k]}
+                'result topDown':result_topDown[k], 'time topDown':time_topDown[k],\
+                'result bottomUp':result_bottomUp[k], 'time bottomUp':time_bottomUp[k]}
             k += 1
         if not path.isdir('../result'):
             if system() == 'Linux': mkdir('../result')            
             elif system() == 'Windows': mkdir('../result')
             elif system() == 'Darwin': mkdir('../result') 
         with open('../result/result'+str(n)+'.json','w') as file: file.write(json.dumps(data,indent=4))
+        print('Generated {}/{} result done!'.format(n,n_result))
     print('\nSuccess!')    
     print("run '$ python3 plot.py' to see the graphics\n")
